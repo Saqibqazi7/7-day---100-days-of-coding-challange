d6215918c13fdae4ae95d46bc22d5908655efc6b
Saqibqazi7 <160391573+Saqibqazi7@users.noreply.github.com>
2024-03-23 04:18:59 +0500

    Add files via upload

---
+++ day7.js
@@ -0,0 +1,6 @@
+//Question no 19
+/*let guests: string[] = ["Saqib", "fahad", "ehan", "Usman"];
+console.log("i'm inviting", guests, "to dinner");*/
+//Question no 20
+var countries = ["England", "Turkey", "U.S.A", "China", "Russia"];
+console.log("countries i'd like to visit:", countries);
---
+++ day7.ts
@@ -0,0 +1,8 @@
+//Question no 19
+/*let guests: string[] = ["Saqib", "fahad", "ehan", "Usman"];
+console.log("i'm inviting", guests, "to dinner");*/
+
+//Question no 20
+let countries: string[] =["England", "Turkey", "U.S.A", "China", "Russia"];
+console.log("countries i'd like to visit:", countries);
+
